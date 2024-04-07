a05e7d404e562ff907be92cf77f51c10bb1360c3
Thiha Naing 27 <thihanaing272023@gmail.com>
2024-04-07 14:27:54 +0630

    world

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex justify-center items-center w-[100vw] h-[100vh] bg-green-500">
       <div>
-        <h1>hello</h1>
+        <h1>hello world</h1>
       </div>
     </div>
   );

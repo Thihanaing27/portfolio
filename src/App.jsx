222a34515abf9826b89387ecbc0ff5e974b66ded
Thiha Naing 27 <thihanaing272023@gmail.com>
2024-04-07 15:20:22 +0630

    fix: Welcome

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex justify-center items-center w-[100vw] h-[100vh] bg-green-500">
       <div>
-        <h1>hello world</h1>
+        <h1 className="text-3xl font-semibold tracking-widest text-red-500">Welcome</h1>
       </div>
     </div>
   );

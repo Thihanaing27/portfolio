9db3bb71d504c5dd8c8688ca43cff9e61aa3a3bf
Thiha Naing 27 <thihanaing272023@gmail.com>
2024-04-07 14:27:08 +0630

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center w-[100vw] h-[100vh] bg-green-500">
+      <div>
+        <h1>hello</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
